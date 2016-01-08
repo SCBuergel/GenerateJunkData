b7cbceddd84b69b58882fedcfaabe51ab0280042
Sebastian Bürgel <s.buergel@gmail.com>
2016-01-08 12:02:15 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace junkData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxNumFiles = new System.Windows.Forms.TextBox();
+            this.textBoxFileSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(112, 77);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(100, 23);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxNumFiles
+            // 
+            this.textBoxNumFiles.Location = new System.Drawing.Point(112, 6);
+            this.textBoxNumFiles.Name = "textBoxNumFiles";
+            this.textBoxNumFiles.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumFiles.TabIndex = 1;
+            this.textBoxNumFiles.Text = "10";
+            // 
+            // textBoxFileSize
+            // 
+            this.textBoxFileSize.Location = new System.Drawing.Point(112, 39);
+            this.textBoxFileSize.Name = "textBoxFileSize";
+            this.textBoxFileSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFileSize.TabIndex = 2;
+            this.textBoxFileSize.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "number of files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "file size (KB)";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 109);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(199, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 144);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFileSize);
+            this.Controls.Add(this.textBoxNumFiles);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxNumFiles;
+        private System.Windows.Forms.TextBox textBoxFileSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace junkData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            int fileSize = 0;
+            int numFiles = 0;
+            int.TryParse(textBoxFileSize.Text, out fileSize);
+            int.TryParse(textBoxNumFiles.Text, out numFiles);
+            Random rnd = new Random();
+            Byte[] b = new Byte[fileSize * 1024];
+
+            string folder = "junkData";
+            Directory.CreateDirectory(folder);
+            string filePrefix = "file";
+            progressBar1.Visible = true;
+            for (int c = 0; c < numFiles; c++)
+            {
+                string filename = folder + "\\" + filePrefix + c.ToString("D9");
+                BinaryWriter bw = new BinaryWriter(File.Open(filename, FileMode.Create));
+                rnd.NextBytes(b);
+                bw.Write(b);
+                bw.Flush();
+                bw.Close();
+                if (c % 10 == 0)
+                    progressBar1.Value = (int)((double)c * (progressBar1.Maximum - progressBar1.Minimum) / numFiles);
+            }
+            progressBar1.Visible = false;
+        }
+    }
+}
